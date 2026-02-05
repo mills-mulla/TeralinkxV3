9b39b46544f7015b1518cbaadbb3424fb03114d8
mills-mulla <sammymulla42@gmail.com>
2026-02-05 14:26:33 +0300

    modified:   teralinkx/apps/finance/authentications.py

--- teralinkx/apps/finance/authentications.py
+++ teralinkx/apps/finance/authentications.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken
 
